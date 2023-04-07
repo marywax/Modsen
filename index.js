9f104f05478eda7180577c6ccb396da63669e40f
Maryna Sukhadolava <maryna.sukhadolava@mail.com>
2023-04-07 16:33:51 +0300

    add an array of questions

--- index.js
+++ index.js
@@ -47,3 +47,56 @@ class Result
    //Логика
 
 }
+
+const questions =
+[
+   new Question("How many planets are in the solar system?",
+   [
+       new Answer("8", 1),
+       new Answer("9", 0),
+       new Answer("10", 0),
+   ]),
+   
+   new Question("What is the freezing point of water?",
+   [
+       new Answer("0", 1),
+       new Answer("-5", 0),
+       new Answer("-6", 0),
+   ]),
+
+   new Question("What is the longest river in the world?",
+   [
+       new Answer("Nile", 1),
+       new Answer("Amazon", 0),
+       new Answer("Yangtze", 0),
+   ]),
+
+   new Question("How many chromosomes are in the human genome?",
+   [
+       new Answer("42", 0),
+       new Answer("44", 0),
+       new Answer("46", 1),
+   ]),
+
+   new Question("Which of these characters are friends with Harry Potter?",
+   [
+       new Answer("Ron Weasley", 1),
+       new Answer("Draco Malfoy", 0),
+       new Answer("Hermione Granger", 2),
+   ]),
+
+   new Question("What is the capital of Canada?",
+   [
+       new Answer("Toronto", 0),
+       new Answer("Ottawa", 1),
+       new Answer("Vancouver", 0),
+   ]),
+   
+   new Question("What is the Jewish New Year called?",
+   [
+       new Answer("Hanukkah", 0),
+       new Answer("Yom Kippur", 1),
+       new Answer("Kwanzaa", 0),
+   ]),
+];
+ 

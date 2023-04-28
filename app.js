d8356d212ae70d9cb2bd114ec5a13a289c6277f0
Maryna Sukhadolava <maryna.sukhadolava@mail.com>
2023-04-28 22:56:07 +0300

    f

--- app.js
+++ app.js
@@ -1,100 +1,122 @@
-const questions = [
+const quizArray = [
     {
-        question:"How many planets are in the solar system?",
-        answer:["8", "9", "10"],
-        correct: 1,
-    },
-    
-    {
-        question:"What is the freezing point of water?",
-        answer:["0", "-5", "-6"],
-        correct: 1,
+        id: "0",
+        question: "How many planets are in the solar system?",
+        options: ["8", "9", "10"],
+        correct: "8",
     },
 
     {
-        question:"What is the longest river in the world?",
-        answer:["Nile", "Amazon", "Yangtze"],
-        correct: 2,
+        question: "What is the freezing point of water?",
+        options: ["0", "-5", "-6"],
+        correct: "0",
     },
 
     {
-        question:"How many chromosomes are in the human genome?",
-        answer:["42","44","46"],
-        correct: 3,
+        question: "What is the longest river in the world?",
+        options: ["Nile", "Amazon", "Yangtze"],
+        correct: "Amazon",
     },
 
     {
-        question:"Which of these characters are friends with Harry Potter?",
-        answer:["Ron Weasley","Draco Malfoy","Hermione Granger"],
-        correct: [1,3]
+        question: "How many chromosomes are in the human genome?",
+        options: ["42", "44", "46"],
+        correct: "46",
     },
 
     {
-        question:"What is the capital of Canada",
-        answer:["Toronto","Ottawa","Vancouver"],
-        correct: 2,
+        question: "Which of these characters are friends with Harry Potter?",
+        options: ["Ron Weasley", "Draco Malfoy", "Hermione Granger"],
+        correct: ["Ron Weasley",  "Hermione Granger"]
     },
 
     {
-        question:"What is the Jewish New Year called?",
-        answer:["Hanukkah","Yom Kippur","Kwanzaa"],
-        correct: 2,
+        question: "What is the capital of Canada",
+        options: ["Toronto", "Ottawa", "Vancouver"],
+        correct: "Ottawa",
+    },
+
+    {
+        question: "What is the Jewish New Year called?",
+        options: ["Hanukkah", "Yom Kippur", "Kwanzaa"],
+        correct: "Yom Kippur",
     }
 ]
 
-const titleContainer = document.querySelector(".title");
-const answersContainer = document.querySelector(".answers");
-const submitButton = document.querySelector(".submit");
-const hintTContainer = document.querySelector(".hint");
+let quizContainer = document.getElementById("container");
+let nextBtn = document.getElementById("next-button");
+let countOfQuestion = document.querySelector(".number-of-question");
+let displayContainer = document.getElementById("display-container");
+let questionCount;
+let scoreCount = 0;
 
+nextBtn.addEventListener(
+    "click",
+    (displayNext = () => {
+        questionCount += 1;
+        if (questionCount == quizArray.length) {
+            displayContainer.classList.add("hide");
+            scoreContainer.classList.remove("hide");
+            userScore.innerHTML =
+                "Your score is " + scoreCount + " out of " + questionCount;
+        } else {
+            countOfQuestion.innerHTML =
+                questionCount + 1 + " / " + quizArray.length + " Question";
+            quizDisplay(questionCount);
+            count = 11;
+        }
+    })
+);
 
-
-let score = 0;
-let questionIndex = 0;
-
-clearPage();
-showQuestion();
-submitButton.onclick=checkAnswer;
-
-
-function clearPage(){
-    titleContainer.innerHTML = "";
-    answersContainer.innerHTML="";
-    hintTContainer.innerHTML="";
+const quizDisplay = (questionCount) => {
+    let quizCards = document.querySelectorAll(".container-mid");
+    quizCards.forEach((card) => {
+        card.classList.add("hide");
+    });
+    quizCards[questionCount].classList.remove("hide");
 };
-
-function showQuestion(){
-    //Question
-    const headerTemplate = ` <h1 class="title">%title%</h1>`;
-    const title = headerTemplate.replace("%title%",questions[questionIndex]['question']);
-    titleContainer.innerHTML=title;
-
-    //Answers
-    let answerNumber = 1;
-
-    for(let item of (questions[questionIndex]['answer'])){
-        const answerTemplate =`
-        <li>
-        <label>
-            <input value = '%number%' type="checkbox" class="answer">
-            <span>%answer%</span>
-        </label>
-        </li>`;
-
-        let answerHTML = answerTemplate
-                                    .replace("%answer%",item)
-                                    .replace("%number%",answerNumber);
-
-        answersContainer.innerHTML += answerHTML;
-        answerNumber++;
+function quizCreator() {
+    console.log("ddd")
+    for (let i of quizArray) {
+        let div = document.createElement("div");
+        div.classList.add("container-mid", "hide");
+        countOfQuestion.innerHTML = 1 + " / " + quizArray.length + " Question";
+        let question_DIV = document.createElement("p");
+        question_DIV.classList.add("question");
+        question_DIV.innerHTML = i.question;
+        div.appendChild(question_DIV);
+        div.innerHTML += `
+    <button class="option-div" onclick="checker(this)">${i.options [0]}</button>
+     <button class="option-div" onclick="checker(this)">${i.options [1]}</button>
+      <button class="option-div" onclick="checker(this)">${i.options [2]}</button>
+    `;
+        quizContainer.appendChild(div);
     }
 }
+function checker(userOption) {
+    let userSolution = userOption.innerText;
+    let question =
+      document.getElementsByClassName("container-mid")[questionCount];
+    let options = question.querySelectorAll(".option-div");
 
-function checkAnswer(){
-    const checkedCheckbox = answersContainer.querySelector('input:checked');
-    console.log(checkedCheckbox);
-    if(!checkedCheckbox){
-        submitButton.blur();
-        return;
+    if (userSolution === quizArray[questionCount].correct) {
+      userOption.classList.add("correct");
+      scoreCount++;
+    } else {
+      userOption.classList.add("incorrect");
+      options.forEach((element) => {
+        if (element.innerText == quizArray[questionCount].correct) {
+          element.classList.add("correct");
+        }
+      });
     }
 }
+function initial() {
+    quizContainer.innerHTML = "";
+    questionCount = 0;
+    scoreCount = 0;
+    count = 11;
+    quizCreator();
+    quizDisplay(questionCount);
+}
+initial();
